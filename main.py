7dbe274e1f8d8bc8308e8a7bf9985bb6ed3eac48
JanDore7 <jan_dore2030@proton.me>
2023-10-25 06:17:49 +0300

    @staticmethod

--- main.py
+++ main.py
@@ -14,10 +14,16 @@ class Vector:
             self.x = x
             self.y = y
 
+        print(self.norm2(self.x, self.y))
+
     def get_cord(self):
         return self.x, self.y
 
+    @staticmethod
+    def norm2(x, y):
+        return x*x + y*y
 
-v = Vector(10, 11)
-print(v.get_cord())
+
+v = Vector(100, 11)
+
 
