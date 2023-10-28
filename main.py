d102bdba8ce464343fd833b0f6e0141b156264b6
JanDore7 <jan_dore2030@proton.me>
2023-10-28 04:25:57 +0300

    приватный метод

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
 class Point:
     def __init__(self, x=0 , y=0):
-        self.__x = x
-        self.__y = y
-
-    def se_cord(self, x, y):
-        if type(x) in (int, float) and type(y) in (int, float):
+        self.__x = self.__y = 0
+        if self.__check_value(x) and self.__check_value(y):
             self.__x = x
             self.__y = y
-        raise ValueError("Координаты должны быть числами")
+
+    @classmethod
+    def __check_value(cls, x):
+        return type(x) in (int, float)
+
+    def set_cord(self, x, y):
+        if self.__check_value(x) and self.__check_value(y):
+            self.__x = x
+            self.__y = y
+        else:
+            raise ValueError("Координаты должны быть числами")
 
     def get_cord(self):
         return self.__x, self.__y
 
 
+p = Point(2, 3)
+p.set_cord(10, 20)
+print(p.get_cord())
